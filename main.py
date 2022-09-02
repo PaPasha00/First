cc9d3c95bed885fcbcccb314012b00fe57ec695a
PaPasha00 <vasev-2020@bk.ru>
2022-09-02 13:13:52 +0300

    Checking for the correctness of parentheses

---
+++ main.py
@@ -0,0 +1,13 @@
+def valid_parentheses(string):
+    count = 0
+    for i in string:
+        if i == "(":
+            count += 1
+        if i == ")":
+            count -= 1
+        if count < 0:
+            return False
+    if count < 0:
+        return False
+    else:
+        return True
